71fd20c0922466b07bcca971f4d2948f7555cafd
Filipe Sales <eduardojs999@gmail.com>
2022-12-23 15:36:35 -0500

    feat: creating dto

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsController } from './reports.controller';
+import { Reports } from './reports.entity';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reports])], //creates the repository ,
   controllers: [ReportsController],
   providers: [ReportsService]
 })
