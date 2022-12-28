b621e257a502a58eaa48c418bfc7b3b58a0ff330
Filipe Sales <eduardojs999@gmail.com>
2022-12-28 07:13:25 -0500

    feat: creating interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,9 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +24,10 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('Handler is running');
     const user = await this.usersService.findOne(parseInt(id));
 
     if (!user) {
