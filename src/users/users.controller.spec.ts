9c12bfc631881bf36c8624e107143aced3940ff0
Filipe Sales <eduardojs999@gmail.com>
2023-01-10 08:09:42 -0500

    feat: auth service unitary test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {};
+    fakeAuthService = {};
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
