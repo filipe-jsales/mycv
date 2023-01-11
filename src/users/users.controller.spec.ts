7ba7986324c2c2dbf9ea9434a0ada7967e0a495c
Filipe Sales <eduardojs999@gmail.com>
2023-01-11 09:05:12 -0500

    feat: starting e2e tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -26,7 +26,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -64,5 +66,15 @@ describe('UsersController', () => {
   it('findUser throws an error if user is not found', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('3')).rejects.toThrow('user not found');
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = {userId: -10};
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1)
   });
 });
