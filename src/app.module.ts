638d9a90aa52623a0207c218deb15830f1e26479
Filipe Sales <eduardojs999@gmail.com>
2023-01-13 07:18:32 -0500

    feat: different databases for testing application

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,26 @@ import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Reports } from './reports/reports.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Reports],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Reports]
+        }
+      }
     }),
     UsersModule,
     ReportsModule,
