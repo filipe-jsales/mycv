fa19cfaba4d481ca5046960d9e60c178e7a616c7
Filipe Sales <eduardojs999@gmail.com>
2023-01-17 07:22:00 -0500

    feat: authentication test finished

---
+++ test/setup.ts
@@ -0,0 +1,18 @@
+import { rm } from 'fs';
+import { join } from 'path';
+
+global.beforeEach(async () => {
+  try {
+    await rm(
+      join(__dirname, '..', 'test.sqlite'),
+      { recursive: true },
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('deleted');
+        }
+      },
+    );
+  } catch (err) {}
+});
