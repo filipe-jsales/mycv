ab002d52b0663f37e414e1cb093be75ae9419b4b
Filipe Sales <eduardojs999@gmail.com>
2023-03-01 07:38:41 -0500

    test lint

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -11,23 +11,23 @@ const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private   usersService: UsersService) {}
 
   async signup(email: string, password: string) {
     //see if email is in use
     const users = await this.usersService.find(email);
     if (users.length) {
-      throw new BadRequestException('email in use');
+          throw new BadRequestException('email in use')
     }
     //Hash the users password
     //Generate a salt
-    const salt = randomBytes(8).toString('hex');
+    const salt = randomBytes(8).toString('hex')
 
     //Hash the salt and the password together
     const hash = (await scrypt(password, salt, 32)) as Buffer;
 
     //Join the hashed result and the salt together
-    const result = salt + '.' + hash.toString('hex');
+      const result = salt + '.' + hash.toString('hex')
 
     //Create a new user and save it
     const user = await this.usersService.create(email, result);
@@ -36,7 +36,7 @@ export class AuthService {
     return user;
   }
 
-  async signin(email: string, password: string) {
+      async signin(email: string, password: string) {
     const [user] = await this.usersService.find(email);
 
     if (!user) {
