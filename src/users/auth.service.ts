b6521187484178172f24fc738c1a7039cd0ea17e
Filipe Sales <eduardojs999@gmail.com>
2023-03-02 07:46:02 -0500

    teste

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -2,10 +2,10 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { randomBytes, scrypt as _scrypt } from 'crypto';
-import { promisify } from 'util';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
 
 const scrypt = promisify(_scrypt);
 
@@ -18,17 +18,17 @@ export class AuthService {
     const users = await this.usersService.find(email);
 
     if (users.length) {
-      throw new BadRequestException('email in use');
+      throw new BadRequestException("email in use");
     }
     //Hash the users password
     //Generate a salt
-    const salt = randomBytes(8).toString('hex');
+    const salt = randomBytes(8).toString("hex");
 
     //Hash the salt and the password together
     const hash = (await scrypt(password, salt, 32)) as Buffer;
 
     //Join the hashed result and the salt together
-    const result = salt + '.' + hash.toString('hex');
+    const result = salt + "." + hash.toString("hex");
 
     //Create a new user and save it
     const user = await this.usersService.create(email, result);
@@ -41,27 +41,17 @@ export class AuthService {
     const [user] = await this.usersService.find(email);
 
     if (!user) {
-      throw new NotFoundException('user not found');
+      throw new NotFoundException("user not found");
     }
 
-    const [salt, storedHash] = user.password.split('.');
+    const [salt, storedHash] = user.password.split(".");
 
     const hash = (await scrypt(password, salt, 32)) as Buffer;
 
-    if (hash.toString('hex') !== storedHash) {
-      throw new BadRequestException('Bad password');
+    if (hash.toString("hex") !== storedHash) {
+      throw new BadRequestException("Bad password");
 
-
-
-
-
-
-
-
-
-
-
-      
+      //teste
     }
     //sigin successfull
     return user;
