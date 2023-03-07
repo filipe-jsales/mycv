313a0d2600d376f2e720ba6d8401e40fd2c9cd49
Filipe Sales <eduardojs999@gmail.com>
2023-03-07 07:01:21 -0500

    feat: get estimate dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,19 +5,27 @@ import {
   Patch,
   Post,
   UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from '../guards/auth.guard';
-import { User } from '../users/user.entity';
-import { CurrentUser } from '../users/decorators/current-user.decorator';
-import { CreateReportDto } from './dtos/create-report.dto';
-import { ReportsService } from './reports.service';
-import { Serialize } from '../interceptors/serialize.interceptor';
-import { ReportDto } from './dtos/report.dto';
-import { ApproveReportDto } from './dtos/approve-report.dto';
+  Get,
+  Query,
+} from "@nestjs/common";
+import { AuthGuard } from "../guards/auth.guard";
+import { User } from "../users/user.entity";
+import { CurrentUser } from "../users/decorators/current-user.decorator";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportsService } from "./reports.service";
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { ReportDto } from "./dtos/report.dto";
+import { ApproveReportDto } from "./dtos/approve-report.dto";
+import { AdminGuard } from "src/guards/admin.guard";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
-@Controller('reports')
+@Controller("reports")
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
+
   @Post()
   @UseGuards(AuthGuard)
   @Serialize(ReportDto)
@@ -25,8 +33,9 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
-  @Patch('/:id')
-  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+  @Patch("/:id")
+  @UseGuards(AdminGuard)
+  approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
 }
