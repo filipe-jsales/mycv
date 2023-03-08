2af0ea964b96da4921a0ee64083d97a0e0c24437
Filipe Sales <eduardojs999@gmail.com>
2023-03-08 07:18:29 -0500

    feat: migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
-import { APP_PIPE } from '@nestjs/core';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { config } from 'process';
-const cookieSession = require('cookie-session');
+import { MiddlewareConsumer, Module, ValidationPipe } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { ReportsModule } from "./reports/reports.module";
+import { User } from "./users/user.entity";
+import { Report } from "./reports/report.entity";
+import { APP_PIPE } from "@nestjs/core";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { config } from "process";
+const cookieSession = require("cookie-session");
 
 @Module({
   imports: [
@@ -21,13 +21,14 @@ const cookieSession = require('cookie-session');
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
         return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
+          type: "sqlite",
+          database: config.get<string>("DB_NAME"),
           synchronize: true,
           entities: [User, Report],
         };
       },
     }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     ReportsModule,
   ],
@@ -43,13 +44,15 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfasfd'],
+          keys: [this.configService.get("COOKIE_KEY")],
         })
       )
-      .forRoutes('*');
+      .forRoutes("*");
   }
 }
